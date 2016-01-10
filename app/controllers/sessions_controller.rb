5116afc4c3d7f42ec465a03d4b26feab37c1a12d
RihardsT <richitislv@gmail.com>
2016-01-10 18:49:45 +0200

    Add most stuff regarding sessions
    Mostly taken from railstutorial.org

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user && user.authenticate(params[:session][:password])
+    	log_in user
+			redirect_back_or root_url
+		else
+			flash.now[:danger] = 'Invalid name/password combination'
+			render 'new'
+		end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+  
 end
