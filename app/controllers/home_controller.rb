c5c1d13917619a68149048248d7217dc1e1cef4f
RihardsT <richitislv@gmail.com>
2016-02-07 20:40:27 +0200

    Improve navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@ class HomeController < ApplicationController
     if current_user.nil?
       redirect_to login_path
     elsif admin_login?
-      @form = 'admin'
+      @page_title = "Admin"
       @logs = Log.all
     elsif logged_in?
-      @form = 'user'
+      @page_title = "User"
       @logs = Log.where(user_id: session[:user_id])
     end
   end
